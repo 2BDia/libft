7a80503e9967ef1af8eb5cc0fb4a26f2b3ff20a3
Robin Van audenhaege <rvan-aud@fu-r8-p5.s19.be>
2021-04-05 11:30:04 +0200

    clean up of extra functions (wrong pdf) + memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -5,27 +5,38 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rvan-aud <rvan-aud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/03 16:51:00 by rvan-aud          #+#    #+#             */
-/*   Updated: 2021/04/03 16:51:00 by rvan-aud         ###   ########.fr       */
+/*   Created: 2021/04/05 10:01:26 by rvan-aud          #+#    #+#             */
+/*   Updated: 2021/04/05 10:54:20 by rvan-aud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char	*srccpy;
 	char	*destcpy;
-	char	*buffer;
+	char	*srccpy;
 	size_t	i;
 
-	srccpy = (char *)src;
 	destcpy = (char *)dest;
-	i = 0;
-	while (i < n)
+	srccpy = (char *)src;
+	if (destcpy > srccpy)
 	{
-		destcpy[i] = srccpy[i];
-		i++;
+		i = n;
+		while (i > 0)
+		{
+			destcpy[i - 1] = srccpy[i - 1];
+			i--;
+		}
 	}
-	return (dest);
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			destcpy[i] = srccpy[i];
+			i++;
+		}
+	}
+	return (destcpy);
 }
