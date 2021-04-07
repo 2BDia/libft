56ec53b7868735743e953c593a99e169de444446
Julie Van Audenhaege <julie.vanaudenhaege@hotmail.com>
2021-04-07 10:51:54 +0200

    strtrim, itoa

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -11,15 +11,47 @@
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 #include "libft.h"
+
+static int	isinset(char const *set, char c)
+{
+	int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	int		i;
+	int		j;
+	int		k;
 	int		lens1;
-	int		lenset;
+	char	*str;
 
 	lens1 = ft_strlen(s1);
-	lenset = ft_strlen(set);
-	
+	i = 0;
+	k = 0;
+	while (s1[i] && isinset(set, s1[i]) == 1)
+		i++;
+	j = lens1 - 1;
+	while (j > 0 && isinset(set, s1[j]) == 1)
+		j--;
+	str = (char *)malloc(sizeof(char) * (((j + 1) - i) + 1));
+	if (!str)
+		return (NULL);
+	while (k < (j + 1) - i)
+	{
+		str[k] = s1[i + k];
+		k++;
+	}
+	str[k] = '\0';
+	return (str);
 }
