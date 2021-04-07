6eb7506c569b4a7f4bb777b7b736c6d17ae9e72a
Robin Van audenhaege <rvan-aud@fu-r8-p5.s19.be>
2021-04-07 10:55:33 +0200

    removed bad header in trim, updated makefile

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
 #include "libft.h"
 
 static int	isinset(char const *set, char c)
