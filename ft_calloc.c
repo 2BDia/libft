b99ba0ccc3b63790e4d34230e5c7d5f57013140f
Julie Van Audenhaege <julie.vanaudenhaege@hotmail.com>
2021-04-13 18:17:43 +0200

    calloc fix

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,12 +6,12 @@
 /*   By: rvan-aud <rvan-aud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 15:58:40 by rvan-aud          #+#    #+#             */
-/*   Updated: 2021/04/13 18:12:55 by rvan-aud         ###   ########.fr       */
+/*   Updated: 2021/04/13 18:17:32 by rvan-aud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <libft.h>
+#include "libft.h"
 
 void	*ft_calloc(size_t count, size_t size)
 {
