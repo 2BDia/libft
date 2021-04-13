793b07685501d39a659d97f3327f2bec6878a6f7
Julie Van Audenhaege <julie.vanaudenhaege@hotmail.com>
2021-04-13 19:08:54 +0200

    tried to fix calloc and strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: rvan-aud <rvan-aud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 15:09:16 by rvan-aud          #+#    #+#             */
-/*   Updated: 2021/04/09 14:01:38 by rvan-aud         ###   ########.fr       */
+/*   Updated: 2021/04/13 19:07:59 by rvan-aud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,19 @@ size_t	ft_strlcat(char *dst, const char *src, size_t n)
 	size_t	lensrc;
 	size_t	lendst;
 
-	i = 0;
 	j = 0;
 	lensrc = ft_strlen(src);
 	lendst = ft_strlen(dst);
-	while (dst[i] != '\0')
-		i++;
-	while (src[j] != '\0' && i + 1 < n)
-		dst[i++] = src[j++];
-	dst[i] = '\0';
-	if (n >= lendst)
+	if (lendst == 0)
+		return (lensrc);
+	if (n < lendst)
 		return (lensrc + lendst);
-	else if (n <= lendst)
-		return (n + lensrc);
-	return (i);
+	i = lendst;
+	while (src[j] && j + lendst < n)
+		dst[i++] = src[j++];
+	if ((lendst + j) == n && lendst < n)
+		dst[--i] = '\0';
+	else
+		dst[i] = '\0';
+	return (lensrc + lendst);
 }
